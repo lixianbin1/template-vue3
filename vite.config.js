15c5f8f1f1a6026b295c01ca037008fbfd7bf4ab
李贤斌 <xianbin.me@qq.com>
2023-11-20 14:48:27 +0800

    config:配置服务显示本地IP

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 本地服务
+  server:{
+    open:true,
+    port:5173,
+    host:"0.0.0.0"
+  },
 })
